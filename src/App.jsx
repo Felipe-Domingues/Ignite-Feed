7857b7b546b8c4281b58a4b51f94c9af59ab96ee
Felipe Domingues de Miranda <felipedm215@hotmail.com>
2023-06-07 00:59:17 -0300

    Iterações JSX

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,78 @@ import { Sidebar } from "./components/Sidebar";
 import styles from "./App.module.css";
 import "./global.css";
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://avatars.githubusercontent.com/u/50550793?v=4",
+      name: "Felipe Domingues de Miranda",
+      role: "FullStack Web Developer",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galera 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portfólio. É um projeto que fiz no NLW Spacetime, evento da Rocketseat. O nome do projeto é Cápsula do Tempo 🚀",
+      },
+      {
+        type: "link",
+        content: "👉 Felipe-Domingues/Rocketseat-NLW-Spacetime",
+        link: "https://rocketseat-nlw-spacetime.vercel.app/",
+      },
+      {
+        type: "inlineLink",
+        content: [
+          {
+            text: "#nlw",
+            link: "",
+          },
+          {
+            text: "#novoprojeto",
+            link: "",
+          },
+          {
+            text: "#rocketseat",
+            link: "",
+          },
+        ],
+      },
+    ],
+    publishedAt: new Date("2023-05-22 20:35:00"),
+  },
+
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://avatars.githubusercontent.com/u/2254731?v=4",
+      name: "Diego Fernandes",
+      role: "CTO @ Rocketseat",
+    },
+    content: [
+      { type: "paragraph", content: "Opa!" },
+      {
+        type: "paragraph",
+        content: "Rede social legal 👌👌❤️",
+      },
+      {
+        type: "inlineLink",
+        content: [
+          {
+            text: "#redeSocial",
+            link: "",
+          },
+          {
+            text: "#igniteFeed",
+            link: "",
+          },
+        ],
+      },
+    ],
+    publishedAt: new Date("2023-06-1 14:29:32"),
+  },
+];
+
 export function App() {
   return (
     <div>
@@ -12,14 +84,16 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post
-            author="Felipe Domingues de Miranda"
-            content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae sit, tenetur quaerat maiores animi nam accusamus maxime nulla, doloremque illum amet? Voluptas consequuntur tempora doloribus, fugit deserunt perspiciatis explicabo cum!"
-          />
-          <Post
-            author="Lucas Mol"
-            content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae sit, tenetur quaerat maiores animi nam accusamus maxime nulla, doloremque illum amet? Voluptas consequuntur tempora doloribus, fugit deserunt perspiciatis explicabo cum!"
-          />
+          {posts.map((post) => {
+            return (
+              <Post
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            );
+          })}
         </main>
       </div>
     </div>
--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,8 +1,21 @@
 import { ThumbsUp, Trash } from "@phosphor-icons/react";
 import styles from "./Comment.module.css";
 import { Avatar } from "./Avatar";
+import { useState } from "react";
 
-export function Comment() {
+export function Comment({ content, onDeleteComment }) {
+  const [likeCount, setLikeCount] = useState(0);
+
+  function handleDeleteComment() {
+    onDeleteComment(content);
+  }
+
+  function handleLikeComment() {
+    setLikeCount((state) => {
+      return state + 1;
+    });
+  }
+
   return (
     <div className={styles.comment}>
       <Avatar
@@ -20,17 +33,17 @@ export function Comment() {
               </time>
             </div>
 
-            <button title="Apagar comentário">
+            <button onClick={handleDeleteComment} title="Apagar comentário">
               <Trash size={24} />
             </button>
           </header>
 
-          <p>Legal demais! 🚀🚀</p>
+          <p>{content}</p>
         </div>
         <footer>
-          <button>
+          <button onClick={handleLikeComment}>
             <ThumbsUp />
-            Aplaudir <span>20</span>
+            Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
